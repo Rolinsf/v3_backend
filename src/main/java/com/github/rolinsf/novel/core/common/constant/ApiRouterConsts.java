df72504cd104fd4b05319145e3badf46ae9dc576
“ljq4946” <1743931738@qq.com>
2025-10-03 00:44:23 +0800

    提交文件

---
+++ src/main/java/com/github/rolinsf/novel/core/common/constant/ApiRouterConsts.java
@@ -0,0 +1,54 @@
+package com.github.rolinsf.novel.core.common.constant;
+
+/**
+ * API 路由常量
+ *
+ * @author xiongxiaoyang
+ * @date 2022/5/12
+ */
+public class ApiRouterConsts {
+
+    /**
+     * API请求路径前缀
+     */
+    String API_URL_PREFIX = "/api";
+
+    /**
+     * 前台门户系统请求路径前缀
+     */
+    String API_FRONT_URL_PREFIX = API_URL_PREFIX + "/front";
+
+    /**
+     * 作家管理系统请求路径前缀
+     */
+    String API_AUTHOR_URL_PREFIX = API_URL_PREFIX + "/author";
+
+    /**
+     * 平台后台管理系统请求路径前缀
+     */
+    String API_ADMIN_URL_PREFIX = API_URL_PREFIX + "/admin";
+
+    /**
+     * 首页模块请求路径前缀
+     * */
+    String HOME_URL_PREFIX = "/home";
+
+    /**
+     * 小说模块请求路径前缀
+     * */
+    String BOOK_URL_PREFIX = "/book";
+
+
+    /**
+     * 前台门户首页API请求路径前缀
+     */
+    String API_FRONT_HOME_URL_PREFIX = API_FRONT_URL_PREFIX + HOME_URL_PREFIX;
+
+    /**
+     * 前台门户小说相关API请求路径前缀
+     */
+    String API_FRONT_BOOK_URL_PREFIX = API_FRONT_URL_PREFIX + BOOK_URL_PREFIX;
+
+
+
+}
