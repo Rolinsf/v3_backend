e9d867ab4cb3f18f1d7fb93e30f6274ab8d23cfe
“ljq4946” <1743931738@qq.com>
2025-10-06 02:29:35 +0800

    1

--- src/main/java/com/github/rolinsf/novel/NovelApplication.java
+++ src/main/java/com/github/rolinsf/novel/NovelApplication.java
@@ -1,9 +1,12 @@
 package com.github.rolinsf.novel;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.github.rolinsf.novel.dao.mapper")
 public class NovelApplication {
 
 	public static void main(String[] args) {
